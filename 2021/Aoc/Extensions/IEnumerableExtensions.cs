d61fcdda32f91854fd1926536300071744675fd6
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-12-04 10:09:59 +0000

    Updated 2021 solutions - Day 4

---
+++ 2021/Aoc/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool ContainsAll<T>(this IEnumerable<T> source, IEnumerable<T> values)
+        {
+            return values.All(value => source.Contains(value));
+        }
+    }
+}
